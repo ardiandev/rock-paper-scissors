59a7c689101aedb0242b48b7cecc567282da32dd
Ardian Pradana <ardiansp18@gmail.com>
2023-09-19 21:34:08 +0700

    Create rock paper and scissors game UI

--- app.js
+++ app.js
@@ -1,95 +1,80 @@
-const startBtn = document.getElementById("start-btn");
-let btnText = startBtn.innerText;
-
-const choices = ["rock", "paper", "scissors"];
+//Global variable
+const choices = ["Rock", "Paper", "Scissors"];
+let computerScore = 0;
 let playerScore = 0;
 
-// function game() {
-//   const restart = restartGame();
-//   for (let i = 1; i <= 5; i++) {
-//     playRound(i);
-//   }
-// }
+//Target Elements
+const rockEl = document.querySelector(".rock");
+const paperEl = document.querySelector(".paper");
+const scissorsEl = document.querySelector(".scissors");
 
-const game = async () => {
-  const restart = await restartGame();
-  for (let i = 1; i <= 5; i++) {
-    playRound(i);
+const playerPoint = document.querySelector(".player-score");
+const compPoint = document.querySelector(".comp-score");
+
+const display = document.querySelector(".info");
+
+const infoDiv = document.querySelector(".info-div");
+
+const playerInfo = document.querySelector(".player-choice");
+const compInfo = document.querySelector(".comp-choice");
+
+//Create element
+const restartBtn = document.createElement("button");
+restartBtn.textContent = "Restart";
+// restartBtn.setAttribute("style", "margin: 5px 0;");
+
+function restartGame() {
+  computerScore = 0;
+  playerScore = 0;
+  playerPoint.textContent = playerScore;
+  compPoint.textContent = computerScore;
+  restartBtn.remove();
+  display.textContent = "Click your choice!";
+  playerInfo.textContent = "";
+  compInfo.textContent = "";
+}
+
+restartBtn.addEventListener("click", restartGame);
+
+const playGame = e => {
+  const compChoice = choices[Math.floor(Math.random() * choices.length)];
+  const playerChoice = e.target.textContent;
+
+  if (playerScore === 5 || computerScore === 5) {
+    return;
+  }
+
+  playerInfo.textContent = playerChoice;
+  compInfo.textContent = compChoice;
+
+  if (playerChoice === compChoice) {
+    display.textContent = "It's draw";
+    return;
+  }
+
+  if (
+    (playerChoice === "Rock" && compChoice === "Scissors") ||
+    (playerChoice === "Paper" && compChoice === "Rock") ||
+    (playerChoice === "Scissors" && compChoice === "Paper")
+  ) {
+    playerScore++;
+    playerPoint.textContent = playerScore;
+    display.textContent = `Player won!! ${playerChoice} beats ${compChoice}`;
+  } else {
+    computerScore++;
+    compPoint.textContent = computerScore;
+    display.textContent = `Computer won!! ${compChoice} beats ${playerChoice}`;
+  }
+
+  if (playerScore === 5) {
+    display.textContent = "Game over!! Player win!!";
+    infoDiv.appendChild(restartBtn);
+  } else if (computerScore === 5) {
+    display.textContent = "Game over!! Computer win!!";
+    infoDiv.appendChild(restartBtn);
   }
 };
 
-function restartGame() {
-  if (btnText === "Start") {
-    startBtn.innerText = "Play again";
-    console.log("Play again");
-  }
-}
-
-function playRound(round) {
-  const restart = restartGame();
-  const computerChoice = getComputerChoice();
-  const playerChoice = getPlayerChoice();
-  const winner = getWinner(playerChoice, computerChoice);
-  const display = displayResult(playerChoice, computerChoice, winner, round);
-}
-
-function getComputerChoice() {
-  return choices[Math.floor(Math.random() * choices.length)];
-}
-
-function getPlayerChoice() {
-  let input = prompt("Type rock, paper, or scissors.");
-  while (input === null) {
-    input = prompt(
-      "Please type your choice. Make sure the spelling is correct."
-    );
-  }
-
-  input = input.toLocaleLowerCase();
-  let check = validateInput(input);
-
-  while (check === false) {
-    input = prompt("Check your spelling!");
-
-    while (input === null) {
-      input = prompt(
-        "Please type your choice. Make sure the spelling is correct."
-      );
-    }
-    input = input.toLowerCase();
-    check = validateInput(input);
-  }
-  return input;
-}
-
-function validateInput(playerInput) {
-  return choices.includes(playerInput);
-}
-
-function displayResult(player, computer, winner, round) {
-  console.log(`Round ${round}`);
-  console.log(`Player: ${player}`);
-  console.log(`Computer: ${computer}`);
-  if (winner !== "Draw") {
-    return console.log(`The winner is ${winner}. `);
-  }
-  return console.log("It is Draw");
-}
-
-function getWinner(player, computer) {
-  if (player === computer) {
-    return "Draw";
-  } else if (
-    (player === "rock" && computer === "scissors") ||
-    (player === "paper" && computer === "rock") ||
-    (player === "scissors" && computer === "paper")
-  ) {
-    return "Player";
-  }
-  return "Computer";
-}
-
-startBtn.addEventListener("click", () => {
-  game();
-});
-// game();
+rockEl.addEventListener("click", playGame);
+paperEl.addEventListener("click", playGame);
+scissorsEl.addEventListener("click", playGame);
